31aa03affba4f08ebdae1e739aa9da52966b1a44
Evgeniy <121779561+NightRunnerEB@users.noreply.github.com>
2023-10-05 22:11:23 +0300

    Create HomeWork_4(optional).s

---
+++ HomeWork_4(optional).s
@@ -0,0 +1,30 @@
+.data
+message:    .asciz  "The maximum argument is "    
+.text
+        la 	a0, message
+        li 	a7, 4           # Системный вызов №4 - вывод на консоль
+        ecall
+	li	s1, 1		#результат произведения
+	li	s2, 1		#копия a0 + 1
+	li	a0, 1		#макс значение аргумента
+        jal     fact
+        li      a7 1
+        ecall
+        li      a7 10
+        ecall
+# Подпрограмма вычисления факториала
+fact:   
+	addi    sp sp -4        ## Запасаем ячейку в стеке
+  sw      ra (sp)         ## Сохраняем ra
+        
+  addi	s2, s2, 1
+	mulhu	t1, s1, s2
+	bnez	t1, overFlow
+	mul	s1, s1, s2
+	addi	a0, a0, 1
+	jal	fact
+overFlow:
+        lw      ra (sp)         ## Восстанавливаем ra
+        addi    sp sp 4         ## Восстанавливаем вершину стека
+        ret
+        
