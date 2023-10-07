c30d478dd98b5fbe1b76fdf37eddc50c92c39b97
Evgeniy <121779561+NightRunnerEB@users.noreply.github.com>
2023-10-07 23:36:56 +0300

    HomeWork#5_optional

---
+++ HomeWork#5_optional/calculateSum_opt.s
@@ -0,0 +1,20 @@
+.global calculateSum
+
+.include "macrolib_opt.s"
+
+.text
+calculateSum:    
+        li      a0  0           	# Сумма
+        li      a1  0			#кол-во просуммированных элементов
+        loop:
+        lw      t4 (t0)
+        checkOverFlow			#Макрос - проверка на переполнение
+        
+        continue:
+        addi    a1 a1 1
+        add     a0, a0, t4	 	# Суммируем значения 
+        addi    t0 t0 4
+        addi    t3 t3 -1        	# Уменьшим количество оставшихся элементов на 1
+        bnez    t3 loop      	# Если осталось больше 0
+        
+        ret
---
+++ HomeWork#5_optional/fillArray_opt.s
@@ -0,0 +1,18 @@
+.global fillArray
+
+.include "macrolib_opt.s"
+
+.text
+fillArray:   
+        read_int_a0
+        sw      a0 (t0)         # Запись числа по адресу в t0
+        addi    t0 t0 4         # Увеличим адрес на размер слова в байтах
+        addi    t3 t3 -1        # Уменьшим количество оставшихся элементов на 1
+        bnez    t3 fillArray    # Не равно ли кол-во оставшихся элементов 
+
+        print_str ("--------\n")
+
+        lw	t3 n		 # Число элементов массива
+        la      t0 array
+        
+        ret
